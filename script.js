3d3a256145731997c03c8842c0638c2410b1cf70
Michayla Constantinou <michayla.constantinou@gmail.com>
2021-03-28 12:53:54 +1100

    doing a different way

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
-const quizContainer = document.getElementById('quiz');
-const resultsContainer = document.getElementById('results');
-const submitButton = document.getElementById('submit');
+var quizQuestions = [
+    {
+        prompt: "HTML is a coding language used to make web pages \n (a) True \n (b) False",
+        answer: "a"
+    },
+    
+]
